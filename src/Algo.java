690c7a6c79675e0398144567ecbaff14724df0ee
Oleksii Davydenko <olexdav@gmail.com>
2020-04-16 09:56:50 +0300

    Update Algo.java

--- src/Algo.java
+++ src/Algo.java
@@ -33,6 +33,6 @@ public class Algo implements AM{
         //        channel_res = new BigInteger((String)channels[i].readObject());
         //        result = result.multiply(channel_res);
         //}
-        info.parent.write(arr);
+        info.parent.write((String)arr.size());
     }
 }
