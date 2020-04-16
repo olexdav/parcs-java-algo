85680d5cd70cf798bcdcae96a1fe34a1ae609840
Oleksii Davydenko <olexdav@gmail.com>
2020-04-16 19:35:23 +0300

    Update Algo.java

--- src/Algo.java
+++ src/Algo.java
@@ -5,8 +5,52 @@ import java.util.*;
 public class Algo implements AM{
     public void run(AMInfo info){
         ArrayList<Integer> arr = (ArrayList<Integer>)info.parent.readObject();
-        Collections.sort(arr);
-        info.parent.write(arr);
-        //info.parent.write("Nice!");
+        //Collections.sort(arr);
+        
+        ArrayList<Integer> result = new ArrayList<Integer>();
+        if (arr.size() == 0) {}
+        else if (arr.size() == 1)
+            result.add(arr.get(0));
+        else {
+            int parts = 2;
+            int step = (int)(Math.ceil((float)(arr.size()) / (float)(parts)));
+            point[] points = new point[parts];
+            channel[] channels = new channel[parts];
+            for (int i = 0; i < parts; i++) { // Split tasks between workers
+                int l, r;
+                l = i*step;
+                r = l + step;
+                if (r > arr.size())
+                    r = arr.size();
+                points[i] = info.createPoint();
+                channels[i] = points[i].createChannel();
+                points[i].execute("Algo");
+                channels[i].write(new ArrayList<Integer>(arr.subList(l, r)));
+            }
+            // Gather results
+            ArrayList<Integer>[] channel_res = new ArrayList<Integer>[parts];// = new ArrayList<Integer>();
+            for (int i = 0; i < parts; i++)
+                channel_res[i] = (ArrayList<Integer>)channels[i].readObject();
+            while (true) {
+                bool anything_left = false;
+                ArrayList<Integer> first = new ArrayList<>();
+                for (int i = 0; i < parts; i++) {
+                    if (channel_res[i].size() > 0) {
+                        anything_left = true;
+                        first.add(channel_res[i].get(0));
+                    }
+                    else first.add(9999999999);
+                }
+                if (!anything_left)
+                    break;
+                int chosen = 0;
+                for (int i = 1; i < parts; i++)
+                    if (first.get(i) < first.get(chosen))
+                        chosen = i;
+                result.add(channel_res[chosen].pop());
+            }
+        }
+        
+        info.parent.write(result);
     }
 }
