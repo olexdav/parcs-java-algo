eddda1c6ab482437315a82ce8f88fc396073f2c0
Oleksii Davydenko <olexdav@gmail.com>
2020-04-16 08:53:36 +0300

    Update Algo.java

--- src/Algo.java
+++ src/Algo.java
@@ -7,12 +7,12 @@ import java.math.BigInteger;
 public class Algo implements AM{
     public void run(AMInfo info){
         //long n,r1,r2,r;
-        long result;
+        BigInteger result;
         List<Integer> range = (List<Integer>)info.parent.readObject();
         if (range.size() == 0)
-            result = 1;
+            result = new BigInteger("1");
         else if (range.size() == 1)
-            result = range.get(0);
+            result = new BigInteger(range.get(0));
         else {
             int parts = 2;
             int step = (int)(Math.ceil((float)(range.size()) / (float)(parts)));
@@ -30,7 +30,7 @@ public class Algo implements AM{
                 channels[i].write(new ArrayList<Integer>(range.subList(l, r)));
             }
             //result = 1; 
-            BigInteger result = new BigInteger("1"); // Gather results
+            result = new BigInteger("1"); // Gather results
             BigInteger channel_res;
             for (int i = 0; i < parts; i++)
                 channel_res = new BigInteger((String)channels[i].readObject());
